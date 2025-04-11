a1b56af090ea63a5e9a8a65677802301be1f79a0
ARTHUR HENRIQUE SILVA MARTINS <arthur.martins@alunos.sc.senac.br>
2025-04-11 11:00:37 -0300

    update

--- src/screens/login/LoginScreen.tsx
+++ src/screens/login/LoginScreen.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from "react-native-gesture-handler";
 
 export default function LoginScreen() {
     return (
         <View style={styles.container}>
-          <Text>Opencfff!</Text>
+          <Text style={styles.text}>SIGN IN</Text>
+          <TextInput style={styles.textInput}>KAKKAKAK</TextInput>
+          <TextInput style={styles.textInput}>KAKAKA</TextInput>
           <StatusBar style="auto" />
         </View>
     )
@@ -18,5 +21,13 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    text: {
+        color: '#8d0000',
+        fontWeight: 'bold',
+    },
+    textInput: {
+        color: '#c31208',
+        //width: 150,
+    }
   });
   
